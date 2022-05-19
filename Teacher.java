1a88e32d05b24491386968dfb8148346a2a46c12
muratsari18 <83982092+muratsari18@users.noreply.github.com>
2022-05-19 19:15:45 +0300

    Create Teacher.java

---
+++ Teacher.java
@@ -0,0 +1,12 @@
+public class Teacher {
+    String name;
+    String phone;
+    String branch;
+
+    Teacher (String name, String phone, String branch){
+        this.name = name;
+        this.phone=phone;
+        this.branch=branch;
+
+    }
+}
